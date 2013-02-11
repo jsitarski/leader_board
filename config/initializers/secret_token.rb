ab8fe507c4cf02ee2be55bc1fdbaea531784bd2a
John-Paul Sitarski <john-paul.sitarski@exelisinc.com>
2013-02-11 10:59:49 -0500

    Initial commit of leaderboard redis example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leaderboard::Application.config.secret_token = 'ee3b7ec439425ba5a692f20cc08b6832dab23f7de4339ca7a0618b361b4861e7677822c92df96e13092186c2c70e5805a4a26710cbf851ef604ecc9d13d76280'
